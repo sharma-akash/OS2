395ac488fc9331a1739ba440264f71c1bb0aad64
Rohit Chaudhary <chaudhro@os2.engr.oregonstate.edu>
2017-10-27 15:26:38 -0700

    Starting concurrency2

---
+++ code_repo/assignment2/dining_philosophers.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+void think();
+void get_forks();
+void eat();
+void put_forks();
+
+int main() {
+  while (true) {
+    think();
+    get_forks();
+    eat();
+    put_forks();
+    }
+}
+
+void think() {
+  return;
+}
+
+void get_forks() {
+  return;
+}
+
+void eat() {
+  return;
+}
+
+void put_forks() {
+  return;
+}
